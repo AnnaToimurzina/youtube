0a40c20cb6f2517c9926ad20e113d6c14925f3bc
AnnaToimurzina <2402as@gmail,com>
2023-05-21 21:50:16 +0500

    дописана функция

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,7 +3,7 @@ from src.channel import Channel
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
     moscowpython.print_info()
-
+# AIzaSyDalbhEObqKUPZFaIcUu3DMDk5r3alExc0
     """
 {
   "kind": "youtube#channelListResponse",
@@ -56,3 +56,4 @@ if __name__ == '__main__':
 }
 
     """
+
--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,12 @@
+import json
+import os
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +14,10 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        info = json.dumps(channel, indent=2, ensure_ascii=False)
+        print(info)
